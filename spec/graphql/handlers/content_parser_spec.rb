6d971685b052593fe3c2b2d84b371a43df1971a0
luciiii6 <73104024+luciiii6@users.noreply.github.com>
2022-05-21 16:56:19 +0300

    Add subject to tests

--- spec/graphql/handlers/content_parser_spec.rb
+++ spec/graphql/handlers/content_parser_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Handlers::ContentParser do
+  subject(:url) { Handlers::ContentParser.get_urls('Best thing I found in a while: https://12ft.io/') }
   it "parser correctly the url/s" do
-    content = 'Best thing I found in a while: https://12ft.io/'
-
-    url = Handlers::ContentParser.get_urls(content)
+    
     expect(url).to be_a Array
     expect(url[0]).to eq 'https://12ft.io/'
   end
--- spec/graphql/handlers/metadata_parser_spec.rb
+++ spec/graphql/handlers/metadata_parser_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Handlers::MetadataParser do
+  subject(:attributes) { Handlers::MetadataParser.get_metadata( 'https://12ft.io/' ) }
   it "gets the metadata" do
-    url = 'https://12ft.io/'
 
-    attributes = Handlers::MetadataParser.get_metadata(url)
     expect(attributes).to be_a Hash
     expect(attributes[:url]).to eq 'https://12ft.io/'
   end
